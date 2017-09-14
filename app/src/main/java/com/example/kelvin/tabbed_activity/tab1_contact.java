9c2beebca72d8b0206b4a2d3b6912514f3795a86
Kelvingandhi <30633049+Kelvingandhi@users.noreply.github.com>
2017-09-14 12:59:28 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/kelvin/tabbed_activity/tab1_contact.java
@@ -0,0 +1,21 @@
+package com.example.kelvin.tabbed_activity;
+
+/**
+ * Created by Kelvin on 1/11/2017.
+ */
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class tab1_contact extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1, container, false);
+        return rootView;
+    }
+
+}
